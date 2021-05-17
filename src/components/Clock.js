86e71e1e2348c7737892dfa9b2bb76bc49d16433
Ethan Kaseff <ethan@kaseff.com>
2021-05-17 18:15:30 -0500

    finished strong

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,51 +1,93 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date()
-    };
-  }
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       time: new Date()
+//     };
+//   }
 
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
+//   componentDidMount() {
+//     this.interval = setInterval(this.tick, 1000);
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.interval);
+//   }
 
-  tick = () => {
-    this.setState({ time: new Date() });
-  }
+//   tick = () => {
+//     this.setState({ time: new Date() });
+//   }
 
-  render() {
-    let hours = this.state.time.getHours();
-    let minutes = this.state.time.getMinutes();
-    let seconds = this.state.time.getSeconds();
+//   render() {
+//     let hours = this.state.time.getHours();
+//     let minutes = this.state.time.getMinutes();
+//     let seconds = this.state.time.getSeconds();
 
-    hours = (hours < 10) ? `0${hours}` : hours;
-    minutes = (minutes < 10) ? `0${minutes}` : minutes;
-    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+//     hours = (hours < 10) ? `0${hours}` : hours;
+//     minutes = (minutes < 10) ? `0${minutes}` : minutes;
+//     seconds = (seconds < 10) ? `0${seconds}` : seconds;
 
-    return (
-      <section>
-        <h1>Clock</h1>
-        <div className='clock'>
-          <p>
-            <span>
-              Time:
+//     return (
+//       <section>
+//         <h1>Clock</h1>
+//         <div className='clock'>
+//           <p>
+//             <span>
+//               Time:
+//             </span>
+//             <span>
+//               {hours}:{minutes}:{seconds} PDT
+//             </span>
+//           </p>
+//           <p>Date: {this.state.time.toDateString()}</p>
+//         </div>
+//       </section>
+//     );
+//   }
+// };
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  // const [intervalThing, setIntervalThing] = useState();
+
+  const tick = () => setTime(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+    return () => clearInterval(interval);
+  }, [])
+
+
+
+
+
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  hours = (hours < 10) ? `0${hours}` : hours;
+  minutes = (minutes < 10) ? `0${minutes}` : minutes;
+  seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+  return (
+    <section>
+      <h1>Clock</h1>
+      <div className='clock'>
+        <p>
+          <span>
+            Time:
             </span>
-            <span>
-              {hours}:{minutes}:{seconds} PDT
+          <span>
+            {hours}:{minutes}:{seconds} PDT
             </span>
-          </p>
-          <p>Date: {this.state.time.toDateString()}</p>
-        </div>
-      </section>
-    );
-  }
-};
+        </p>
+        <p>Date: {time.toDateString()}</p>
+      </div>
+    </section>
+  );
+}
 
 export default Clock;
